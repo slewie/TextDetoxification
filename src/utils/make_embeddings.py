42d0e6331741da67391f3f6965f43a95791a0a84
slewie <vladkowww@gmail.com>
2023-11-05 19:13:14 +0300

    download embeddings from the disk

--- src/utils/make_embeddings.py
+++ src/utils/make_embeddings.py
@@ -1,12 +1,24 @@
-import fasttext
-import pandas as pd
+import requests
+from urllib.parse import urlencode
+import os
 
 
 def make_embeddings(data_embeddings_path='../data/interim/toxicity_levels.csv',
-                    embedding_path="../models/fasttext.bin"):
-    df_train = pd.read_csv(data_embeddings_path)
-    df_train['text'].to_csv('data.txt', index=False)
-    print('Creating embeddings...')
-    model = fasttext.train_unsupervised('data.txt')
+                    embedding_path="fasttext.bin"):
+    print('Downloading embeddings...')
+    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+    public_key = 'https://disk.yandex.ru/d/4KOFyzhKjvYUtg'
+
+    final_url = base_url + urlencode(dict(public_key=public_key))
+    response = requests.get(final_url)
+    download_url = response.json()['href']
+
+    download_response = requests.get(download_url, stream=True)
+    with open(os.path.join(os.path.dirname(__file__), '../../models/') + embedding_path, 'wb') as f:
+        for chunk in download_response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
     print('Embeddings created')
-    model.save_model(embedding_path)
+
+
+make_embeddings()
